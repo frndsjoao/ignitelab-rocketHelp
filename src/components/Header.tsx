99dfda2b9866dd660b409e09dfa7dfaa179c8092
João Pedro Assunção <joaofernandes.dev@hotmail.com>
2022-08-01 22:30:23 -0300

    Create screens

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { useNavigation } from '@react-navigation/native';
+import { Heading, HStack, IconButton, StyledProps, useTheme, VStack } from 'native-base';
+import { CaretLeft } from 'phosphor-react-native';
+
+type Props = StyledProps & {
+  title: string;
+}
+
+export function Header({ title, ...rest }) {
+  const { colors } = useTheme()
+  const navigation = useNavigation()
+  return (
+    <HStack
+      w="full"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="gray.600"
+      pb={6}
+      pt={12}
+      {...rest}
+    >
+      <IconButton
+        icon={<CaretLeft color={colors.gray[200]} size={24} />}
+        onPress={() => navigation.goBack()}
+      />
+      <Heading color="gray.100" textAlign="center" fontSize="lg" flex={1} ml={-6}>
+        {title}
+      </Heading>
+
+    </HStack>
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,102 @@
+import { useNavigation } from '@react-navigation/native';
+import { Center, FlatList, Heading, HStack, IconButton, Text, useTheme, VStack } from 'native-base';
+import { ChatTeardropText, SignOut } from 'phosphor-react-native';
+import { useState } from 'react';
+
+import Logo from '../assets/logo_secondary.svg'
+import { Button } from '../components/Button';
+import { Filter } from '../components/Filter';
+import { Order, OrderProps } from '../components/Order';
+
+
+export function Home() {
+  const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>('open')
+  const [orders, setOrders] = useState<OrderProps[]>([
+    {
+      id: '122',
+      patrimony: '12321',
+      when: '01/08/2022 às 21:22',
+      status: 'open'
+    },
+    {
+      id: '123',
+      patrimony: '12321',
+      when: '01/08/2022 às 21:22',
+      status: 'closed'
+    }
+  ])
+  const { colors } = useTheme()
+  const navigation = useNavigation()
+
+  function handleNewOrder() {
+    navigation.navigate('new')
+  }
+
+  function handleOpenDetails(orderId: string) {
+    navigation.navigate('details', { orderId })
+  }
+
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+        />
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack w="full" mt={8} mb={4} justifyContent="space-between" alignItems="center">
+          <Heading color="gray.100">Meus chamados</Heading>
+          <Text color="gray.200">01</Text>
+        </HStack>
+
+        <HStack space={3} mb={8}>
+          <Filter
+            type="open"
+            title='em andamento'
+            onPress={() => (setStatusSelected('open'))}
+            isActive={statusSelected === 'open'}
+          />
+          <Filter
+            type="closed"
+            title='finalizados'
+            onPress={() => (setStatusSelected('closed'))}
+            isActive={statusSelected === 'closed'}
+          />
+        </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <Order data={item} onPress={() => handleOpenDetails(item.id)} />}
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{ paddingBottom: 100 }}
+          ListEmptyComponent={
+            <Center>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                Nenhuma solicitação {statusSelected === 'open' ? 'em andamento' : 'finalizada'}{'\n'}
+                a ser exibido.
+              </Text>
+            </Center>
+          }
+        />
+
+        <Button
+          title="Nova solicitação"
+          onPress={handleNewOrder}
+        />
+
+      </VStack>
+    </VStack>
+  );
+}
